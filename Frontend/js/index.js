07333f1089a7766425bc8a7d0b73be6091612b96
OnMyMind-28 <taliasalvador@yahoo.com>
2022-10-31 21:18:22 -0600

    Frontend-Motoristas-Avance1

---
+++ Frontend/js/index.js
@@ -0,0 +1,129 @@
+///-----------------------------------------------Motorista-------------------------------
+var motorista = [{
+    idMotorista:1,
+    nombre: "Antonio",
+    apellido: "Madrid",
+    /******Pedidos******************************************************************************* */
+    pedidos: [{
+        numeroPedido: 1,
+        puntoRetiro: "Lorem djcnj",
+        puntoEntrega: "Lorem jvskjav",
+        detalleRetiro: "Bulevar",
+        detalleEntrega: "Avenida",
+        detalleOrde: "Combo Familiar Burger King"
+    }
+
+    ],
+    /******Mapa******************************************************************************* */
+    mapa:[{
+        miUbicacion: "Cabañas"
+    }],
+    /******Historial************************************************************************************** */
+    historialEntregas: [{
+        numeroTransaccion: 1,
+        icono: "fa-solid fa-user-group ",
+        horaDeEntrega: 1000
+    },
+        {
+            numeroTrasaccion: 2,
+            horaDeEntrega: 0800
+            
+        }
+    ],
+    /******Caja************************************************************************************** */
+    caja:[{
+        pago: 1,
+        monto: 50,
+        fecha: "15/10/2022",
+        montoPropina: 25,
+        totalPago: 985
+    }]
+},
+ 
+   {
+    idMotorista:2,
+        nombre: "Johan",
+        apellido: "Salvador",
+    /******Pedidos******************************************************************************* */
+        pedidos: [{
+            numeroPedido: 1,
+            puntoRetiro: "Lorem djcnj",
+            puntoEntrega: "Lorem jvskjav",
+            detalleRetiro: "Bulevar",
+            detalleEntrega: "Avenida",
+            detalleOrde: "Combo Familiar Burger King"
+        }
+
+        ],
+        /******Mapa******************************************************************************* */
+        mapa:[{
+            miUbicacion: "Cabañas",
+        }],
+        /******Historial************************************************************************************** */
+        historialEntregas: [{
+            numeroTransaccion: 3 ,
+            icono: "fa-solid fa-user-group ",
+            horaDeEntrega: 1100 
+        },
+            {
+                numeroTransaccion: 4,
+                icono: "fa-solid fa-user-group ",
+                horaDeEntrega: 1300
+                
+            },
+
+        ],
+        /******Caja************************************************************************************** */
+        caja:[{
+            pago: 1,
+            monto: 50,
+            fecha: "15/10/2022",
+            montoPropina: 25,
+            totalPago: 985
+
+        }]
+    },
+    {
+        idMotorista:3,
+        nombre: "Victoria",
+        apellido: "Rodríguez",
+  /******Pedidos******************************************************************************* */
+        pedidos: [{
+            numeroPedido: 1,
+            puntoRetiro: "Lorem djcnj",
+            puntoEntrega: "Lorem jvskjav",
+            detalleRetiro: "Bulevar",
+            detalleEntrega: "Avenida",
+            detalleOrde: "Combo Familiar Burger King"
+        }
+
+        ],
+        /******Mapa******************************************************************************* */
+        mapa:[{
+            miUbicacion: "Cabañas",
+        }],
+        /******Historial************************************************************************************** */
+        historialEntregas: [{
+            numeroTransaccion: 5,
+            icono: "fa-solid fa-user-group ",
+            horaDeEntrega: 1600 
+        },
+            {
+                numeroTransaccion: 6,
+                icono: "fa-solid fa-user-group ",
+                horaDeEntrega: 1500 
+            }
+        ],
+        /******Caja************************************************************************************** */
+        caja:[{
+            pago: 1,
+            monto: 50,
+            fecha: "15/10/2022",
+            montoPropina: 25,
+            totalPago: 985
+
+        }]
+      
+    }
+];
+
